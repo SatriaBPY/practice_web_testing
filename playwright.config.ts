325703f2006ed721f7ba076097796af493a54c6b
Satria Bayu Putra <killusme@gmail.com>
2026-05-14 17:00:50 +0700

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,15 +4,15 @@ export default defineConfig({
   globalSetup: "./setup/global_setup.ts",
   globalTeardown: "./setup/teardown_setup.ts",
   testDir: "./tests",
-  testMatch: [
-    "e2e_login.spec.ts",
-    "login.spec.ts",
-    "product_overview.spec.ts",
-    "product_detail.spec.ts",
-    "checkout_review.spec.ts",
-    "checkout_billing_address.spec.ts",
-    "checkout_payment.spec.ts",
-  ],
+  // testMatch: [
+  //   "e2e_login.spec.ts",
+  //   "login.spec.ts",
+  //   "product_overview.spec.ts",
+  //   "product_detail.spec.ts",
+  //   "checkout_review.spec.ts",
+  //   "checkout_billing_address.spec.ts",
+  //   "checkout_payment.spec.ts",
+  // ],
   
   /* Run tests in files in parallel */
   expect: {
@@ -54,15 +54,44 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
+  // projects: [
+  //   {
+  //     name: "Automation Testing Chrome",
+  //     use: {
+  //       ...devices["Desktop Chrome"],
+  //     },
+  //   },
+  // ],
+  // 
   projects: [
-    {
-      name: "Automation Testing Chrome",
-      use: {
-        ...devices["Desktop Chrome"],
+      {
+        name: 'Login-Suite',
+        testMatch: /login\.spec\.ts/,
       },
-    },
-  ],
-
+      {
+        name: 'Product-Overview',
+        testMatch: /product_overview\.spec\.ts/,
+        dependencies: ['Login-Suite'], 
+      },
+      {
+        name: 'Product-Detail',
+        testMatch: /product_detail\.spec\.ts/,
+        dependencies: ['Product-Overview'],
+      },
+      {
+        name: 'Checkout-Flow',
+        testMatch: [
+          /checkout_review\.spec\.ts/,
+          /checkout_billing_address\.spec\.ts/,
+          /checkout_payment\.spec\.ts/
+        ],
+        dependencies: ['Product-Detail'],
+      },
+      {
+        name: 'E2E-Final-Check',
+        testMatch: /e2e_login\.spec\.ts/,
+        dependencies: ['Checkout-Flow'],
+      },
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
