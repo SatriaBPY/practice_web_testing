61ec3378869078969795b16a9f82199cb93388ab
Satria Bayu Putra <killusme@gmail.com>
2026-05-16 14:09:40 +0700

    Create env.intercaces.ts

---
+++ src/types/env.intercaces.ts
@@ -0,0 +1,12 @@
+export interface EnvironmentConfig {
+  api_base_url: string;
+  base_url: string;
+  admin_email: string;
+  admin_password: string;
+  validLogin: string;
+  validPassword: string;
+  default_user: string;
+  default_password: string;
+  disableLogin: string;
+  disablePassword: string;
+}
